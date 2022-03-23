b57255554cbf005d816c4b24c29c10d970e321a9
pauloricardorc <pauloricardorc0@gmail.com>
2022-03-23 19:18:46 -0400

    prisma configuration with sqlite

---
+++ prisma/migrations/20220323224950_create_user/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
-import type { NextPage } from 'next'
+import { PrismaClient } from "@prisma/client"
+import { GetServerSideProps } from "next"
+import { prisma } from "../lib/prisma"
 
 export default function Home() {
   return (
     <div className='container mx-auto'>
       <div className="grid grid-cols-3 gap-3">
-        <div className="w-50 h-50 bg-blue-200"></div>
+        
       </div>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const users = await prisma.user.findMany()
+
+  return{
+    props: {
+
+    }
+  }
 }
